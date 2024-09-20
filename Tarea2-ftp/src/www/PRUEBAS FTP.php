8f727199e24e0093ea5836338576a4ae4482f0fb
Juan Acuña <juca.acuna@gmail.com>
2024-09-20 12:29:09 -0300

    Se ajustan nombre de carpetas.

---
+++ Tarea2-ftp/src/www/PRUEBAS FTP.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<html>
+
+<head>
+    <?php require 'miHead.php'; ?>
+    <title><?php echo $titulo="ftp"?></title>
+</head>
+
+<body>
+    <?php require 'miMenu.php'; ?>
+    <main>
+		<article>
+            <?php
+            $ftp_server = "IP_DEL_SERVIDOR_FTP";
+            $ftp_username = "juan";
+            $ftp_password = "contraseña_de_ftpinet";
+
+
+            ?>
+		</article>
+        
+    </main>
+
+    <?php require_once 'miFooter.php'; ?>
+</body>
+
+</html>
