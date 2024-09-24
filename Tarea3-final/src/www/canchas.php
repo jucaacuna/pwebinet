b99f4a69121fb61a62849d25981da4275738fbaa
Juan Acuña <juca.acuna@gmail.com>
2024-09-24 19:40:04 -0300

    Se agrega .gitignore

--- Tarea3-final/src/www/canchas.php
+++ Tarea3-final/src/www/canchas.php
@@ -7,8 +7,15 @@
     <title><?php echo $titulo="CANCHAS"?></title>
 </head>
 <body>
+<?php require 'miMenu.php'; ?>
 <main>
   <h1> CANCHAS BIEN PRESENTADAS, CON FOTO. 1 POR ARTICLE</h1>
+  <article>
+			<h2>Cancha 1</h2> <!-- variable en mysql -->
+			<p>Esta cancha, es la mejor de todas.</p> <!-- variable en mysql -->
+      <img src="/imagenes/cancha1.jpg" /> <!-- variable en mysql -->
+
+		</article>
     </main>
 
     <?php require_once 'miFooter.php'; ?>
